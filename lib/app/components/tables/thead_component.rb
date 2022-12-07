b972a27e695270eba435eededcd6d8690ba55ed3
Julien <julien.camblan@gmail.com>
2022-12-07 11:55:33 +0100

    feat: import last changes from graphql_template

--- lib/app/components/tables/thead_component.rb
+++ lib/app/components/tables/thead_component.rb
@@ -3,5 +3,6 @@
 module Tables
   class TheadComponent < ViewComponent::Base
     renders_many :ths, Tables::ThComponent
+    renders_many :active_record_ths, Tables::ActiveRecordThComponent
   end
 end
