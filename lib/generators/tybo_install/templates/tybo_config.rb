0ba845b8179460c602f0b96e79a1541cbac15fe9
dlp-m <michel@tymate.com>
2023-03-15 08:38:10 +0100

    feat: update base img

--- lib/generators/tybo_install/templates/tybo_config.rb
+++ lib/generators/tybo_install/templates/tybo_config.rb
@@ -3,7 +3,7 @@
 Tybo.configure do |config|
   # customise logo and cover url
   # should be an external url or image should be present in (app/assets/images)
-  # config.logo_url = 'logo.png'
-  # config.nav_logo_url = 'nav_logo.png'
-  # config.cover_url = 'cover.png'
+  config.logo_url = 'https://www.datocms-assets.com/33962/1667497145-logo-colored.svg'
+  config.nav_logo_url = 'https://www.datocms-assets.com/33962/1667497145-logo-colored.svg'
+  config.cover_url = 'https://tymate.com/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F33962%2F1671032522-natif-2.jpg%3Fw%3D1600&w=3840&q=75'
 end
