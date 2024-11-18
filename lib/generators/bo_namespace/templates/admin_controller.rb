d8ef82a8f2df325d7a24332d13c17c879ed1cee7
dlp-m <michel@tymate.com>
2024-11-18 08:38:51 +0100

    feat: fix install script

--- lib/generators/bo_namespace/templates/admin_controller.rb
+++ lib/generators/bo_namespace/templates/admin_controller.rb
@@ -3,13 +3,28 @@ require 'csv'
 
 class <%= class_name %>Controller < ApplicationController
   include Pagy::Backend
-  layout '<%= class_name.underscore %>'
-  before_action :authenticate_<%= class_name.underscore %>!
-  authorize :user, through: :current_<%= class_name.underscore %>
+  layout 'administrator'
+  before_action :authenticate_administrator!, :set_default_locale
+  authorize :user, through: :current_administrator
 
   private
 
   def namespace
-    @namespace ||= Bo::<%= class_name.pluralize %>
+    @namespace ||= Bo::Administrators
+  end
+
+  def set_default_locale
+    I18n.default_locale = :fr
+  end
+
+  protected
+
+  # Devise routing overwrite for multiples resources
+  def after_sign_in_path_for(resource)
+    send(:"#{resource.class.name.underscore.pluralize}_root_path")
+  end
+
+  def after_sign_out_path_for(resource)
+    send(:"new_#{resource}_session_path")
   end
 end
