6224d4ef42add57f451fbfa0faa617643de73f6d
dlp-m <delpierre.michel@outlook.fr>
2026-04-01 15:12:51 +0200

    fix: add .js extensions to relative imports in index.js
    Browsers do not resolve relative imports without explicit extensions in
    native ES modules. importmap only handles bare specifiers, not relative
    URLs. Also simplify importmap.rb to only pin the entry point.

--- app/assets/javascripts/tybo/controllers/index.js
+++ app/assets/javascripts/tybo/controllers/index.js
@@ -1,7 +1,7 @@
-export { default as Attachments } from "./attachments_controller";
-export { default as Dropdown } from "./dropdown_controller";
-export { default as Flash } from "./flash_controller";
-export { default as SearchForm } from "./search_form_controller";
-export { default as TsSearch } from "./ts/search_controller";
-export { default as TsSelect } from "./ts/select_controller";
-export { default as Sidebar } from "./sidebar_controller";
+export { default as Attachments } from "./attachments_controller.js";
+export { default as Dropdown } from "./dropdown_controller.js";
+export { default as Flash } from "./flash_controller.js";
+export { default as SearchForm } from "./search_form_controller.js";
+export { default as TsSearch } from "./ts/search_controller.js";
+export { default as TsSelect } from "./ts/select_controller.js";
+export { default as Sidebar } from "./sidebar_controller.js";
